2d240b04e62681dda8b0cb7120472b1aad7b6626
Atul Shimpi <shimpiatul@hotmail.com>
2018-05-21 09:08:37 +0530

    Seperate root and home components

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+
+//    {path: 'products', loadChildren: 'app/html-builder/html-builder.module#HtmlBuilderModule'}
+
+const routes: Routes = [
+    {path: '', component: HomeComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+	AppRoutingModule,
    MDBBootstrapModule.forRoot()
   ],
   providers: [],
